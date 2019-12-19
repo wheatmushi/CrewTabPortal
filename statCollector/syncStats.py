b02d9b764c09f20d4db4c9f42684a2d20da64754
wheatmushi <9000ofhate@gmail.com>
2019-12-19 18:00:01 +0300

    added % in build_stats and temp backups in get_crew_roles

--- statCollector/syncStats.py
+++ statCollector/syncStats.py
@@ -1,4 +1,5 @@
-#  build devices synchronization statistic
+#  build devices synchronization statistic in two views: amount of syncs in given time intervals
+#  and receiving of passengers data
 
 import os
 import sys
@@ -9,9 +10,10 @@ from crew_utils import date_iterator
 from CrewInterface import CrewInterface
 
 
-start_date = '2019-12-02'
-num_of_days = 2
-airports = []  # only build stats for this airports
+start_date = '2019-11-18'
+num_of_days = 30
+dates = [d for d in date_iterator(start_date, num_of_days)]
+airports = ['']  # only build stats for this airports
 
 
 pd.set_option('display.width', 320)
@@ -25,10 +27,10 @@ def check_intervals(dataframe, drop_duplicates=True):  # create interval/passeng
                          (table['arr_airport'] == s['arrivalAirport'])]['duration'].min()
         duration = duration if 30 < duration < 1000 else 50
         difference = s['difference']
-        if -9 <= difference < duration: return 'full'
-        if -38 <= difference < -9: return 'registration'
-        if -4320 <= difference < -38: return 'base'
-        return 'late_data'
+        if -9 <= difference < duration: return 'full'  # full boarding data
+        if -38 <= difference < -9: return 'registration'  # checked in data
+        if -4320 <= difference < -38: return 'base'  # booking data
+        return 'late_data'  # data received after flight end
 
     def passengers(s):
         m = {'full': 'boardedCount', 'registration': 'checkinCount', 'base': 'bookedCount', 'late_data': 'boardedCount'}
@@ -36,11 +38,10 @@ def check_intervals(dataframe, drop_duplicates=True):  # create interval/passeng
         if value_to_check == 0: return 'no_data'
         if value_to_check < 0.7 * int(s['bookedCount']): return 'incorrect'
         return 'ok'
-    df = dataframe.copy()
-    flights_table = pd.read_csv(os.path.join('..', '_DB', 'catering', 'afl_routes.csv'), sep=',')
+    df = dataframe.copy(deep=True)
+    routes_table = pd.read_csv(os.path.join('..', '_DB', 'catering', 'afl_routes.csv'), sep=',')
     df['difference'] = (df['synchronizationDate'] - df['scheduledDepartureDateTime'])/pd.Timedelta(minutes=1)
-    #df['interval'] = df['difference'].map(lambda i: interval(i))
-    df['interval'] = df.apply(lambda s: interval(s, flights_table), axis=1)
+    df['interval'] = df.apply(lambda s: interval(s, routes_table), axis=1)
     df['passengers'] = df.apply(lambda s: passengers(s), axis=1)
     if drop_duplicates:
         df = df.drop_duplicates(['staffId', 'flightNumber', 'scheduledDepartureDateTime', 'interval', 'passengers'])
@@ -56,10 +57,10 @@ def build_stats(interface, dataframe, dates, kind, index, columns, percent_axis=
     # index: rows for stat table (day, hour, departureAirport, staffId, passengers, etc), group by most left first
     # columns: columns for stat table (position, interval, etc), group by most left first
     # percent_axis: 0 for percent count along column, 1 for percent count along row, None for raw numbers
-    df = dataframe.copy()
+    df = dataframe.copy(deep=True)
     if ('position' in index or 'position' in columns) and 'position' not in df.columns:
         df = get_crew_roles(interface, df, dates)
-        df = df[df['position'].isin(('CM', 'FA'))]
+    df = df[df['position'].isin(('CM', 'FA'))]
     df['day'] = df['scheduledDepartureDateTime'].dt.date
     df['hour'] = df['scheduledDepartureDateTime'].dt.hour
 
@@ -90,37 +91,50 @@ def build_stats(interface, dataframe, dates, kind, index, columns, percent_axis=
         df = df.drop_duplicates(['staffId', 'flightNumber', 'departureDate'], keep='first')
 
     stats = df.pivot_table(index=index, columns=columns, aggfunc='size', fill_value=0)
-    if percent_axis:
-        stats = stats.apply(lambda line: round(line / line.sum() * 100, 1), axis=percent_axis)
-    return stats
+    if type(percent_axis) is int:
+        stats_percents = stats.apply(lambda line: round(line / line.sum() * 100, 1), axis=percent_axis)
+        if percent_axis == 1:
+            stats_percents.columns = ['% ' + c for c in stats_percents.columns]
+        elif percent_axis == 0:
+            stats_percents.index = ['% ' + c for c in stats_percents.index]
+        stats = pd.concat([stats_percents, stats], axis=percent_axis)
+    return stats.sort_index(ascending=False)
 
 
 def get_crew_roles(interface, df, dates):  # add crew positions in syncs dataframe, this will slow script with 1
     # additional http-request per every flight in table (about 800 requests for one day)
+    # NEED TO ADD save temp result mechanism for poor internet connection
     t = time()
     check_list = df.drop_duplicates(['flightNumber', 'departureDate'])
+    if 'temp_crews.csv' in os.listdir('.'):
+        if input('load crew data from temporary file? y/n') == 'y':
+            temp_crew_table = pd.read_csv('temp_crews.csv', index_col=1, sep=',')
+            check_list = check_list[~((check_list['flightNumber'].isin(temp_crew_table['flightNumber'])) &
+                                    (check_list['departureDate'].isin(temp_crew_table['departureDate'])))]
     flights = interface.get_flights_table(dates_range=dates)
     print('parsing crew roles for {} flights...'.format(check_list.shape[0]))
     crew_list = []
     for i, row in check_list.iterrows():
-        date = row['scheduledDepartureDateTime'].strftime('%Y-%m-%d')
         flight_id = flights[(flights['flightNumber'] == row['flightNumber']) &
                             (flights['departureDate'] == row['departureDate'])].index[0]
         crews = interface.get_flight_crews(flight_id, log=False)
         crews['scheduledDepartureDateTime'] = row['scheduledDepartureDateTime']
         crews['flightNumber'] = row['flightNumber']
         crew_list.append(crews)
-        if len(crew_list) % 100 == 0:
+        if len(crew_list) % 500 == 0:
             print('completed', len(crew_list), 'flights of', check_list.shape[0])
+            temp_crew_table = pd.concat(crew_list, axis=0, sort=False)
+            temp_crew_table.to_csv('temp_crews.csv')
     crews = pd.concat(crew_list, axis=0, sort=False)
     crews = crews.drop(['name', 'email', 'DT_RowId'], axis=1)
     df = df.merge(crews, how='left', on=['staffId', 'flightNumber', 'scheduledDepartureDateTime'])
+    os.remove('temp_crews.csv')
     print('crew roles parsing time {} seconds'.format(round(time() - t)))
     return df
 
 
 url_main = 'https://admin-su.crewplatform.aero/'
 interface = CrewInterface(url_main)
-df = interface.get_syncs(departure_dates=date_iterator(start_date, num_of_days))
+df = interface.get_syncs(departure_dates=dates, departure_airports=airports)
 df = check_intervals(df)
-df = get_crew_roles(interface, df, date_iterator(start_date, num_of_days))
+df = get_crew_roles(interface, df, dates)
